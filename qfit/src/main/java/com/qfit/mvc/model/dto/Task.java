85e4f43eaecab1940d07724b19a933922f94fafa
austin <tmddnjs1447@naver.com>
2024-11-04 12:59:52 +0900

    Feat : Exercise 추가

--- qfit/src/main/java/com/qfit/mvc/model/dto/Task.java
+++ qfit/src/main/java/com/qfit/mvc/model/dto/Task.java
@@ -5,9 +5,9 @@ public class Task {
 	private int questId;
 	private int orderIndex;
 	private boolean isCompleted;
-	private int count;
-	private int weightKg;
-	private int cardioMinutes;
+	private Integer count;
+	private Integer weightKg;
+	private Integer cardioMinutes;
 	private int exerciseId;
 	
 	/*
@@ -16,8 +16,17 @@ public class Task {
 	public Task() {
 	}
 	
+//	// 전체 생성자
+//	public Task(int questId, boolean isCompleted, Integer count, Integer weightKg, Integer cardioMinutes, int exerciseId) {
+//		this.questId = questId;
+//		this.count = count;
+//		this.weightKg = weightKg;
+//		this.exerciseId = exerciseId;
+//	}
+	
 	// 무산소 생성자
-	public Task(int questId, boolean isCompleted, int count, int weightKg, int cardioMinutes, int exerciseId) {
+	public Task(int questId, Integer count, Integer weightKg, int exerciseId) {
+		super();
 		this.questId = questId;
 		this.count = count;
 		this.weightKg = weightKg;
@@ -25,7 +34,7 @@ public class Task {
 	}
 	
 	// 유산소 생성자
-	public Task(int questId, int cardioMinutes, int exerciseId) {
+	public Task(int questId, Integer cardioMinutes, int exerciseId) {
 		super();
 		this.questId = questId;
 		this.cardioMinutes = cardioMinutes;
@@ -56,22 +65,22 @@ public class Task {
 	public void setCompleted(boolean isCompleted) {
 		this.isCompleted = isCompleted;
 	}
-	public int getCount() {
+	public Integer getCount() {
 		return count;
 	}
-	public void setCount(int count) {
+	public void setCount(Integer count) {
 		this.count = count;
 	}
-	public int getWeightKg() {
+	public Integer getWeightKg() {
 		return weightKg;
 	}
-	public void setWeightKg(int weightKg) {
+	public void setWeightKg(Integer weightKg) {
 		this.weightKg = weightKg;
 	}
-	public int getCardioMinutes() {
+	public Integer getCardioMinutes() {
 		return cardioMinutes;
 	}
-	public void setCardioMinutes(int cardioMinutes) {
+	public void setCardioMinutes(Integer cardioMinutes) {
 		this.cardioMinutes = cardioMinutes;
 	}
 	public int getExerciseId() {
@@ -83,7 +92,7 @@ public class Task {
 
 	@Override
 	public String toString() {
-		return "TaskDto [taskId=" + taskId + ", questId=" + questId + ", orderIndex=" + orderIndex + ", isCompleted="
+		return "Task [taskId=" + taskId + ", questId=" + questId + ", orderIndex=" + orderIndex + ", isCompleted="
 				+ isCompleted + ", count=" + count + ", weightKg=" + weightKg + ", cardioMinutes=" + cardioMinutes
 				+ ", exerciseId=" + exerciseId + "]";
 	}
--- qfit/src/main/java/com/qfit/mvc/model/service/TaskService.java
+++ qfit/src/main/java/com/qfit/mvc/model/service/TaskService.java
@@ -8,16 +8,24 @@ public interface TaskService {
 	
 	// 전체 태스크 조회
 	public List<Task> getTaskList();
-	// 태스크 추가
-	public void addCardioTask(Task task);
-	// 태스크 추가
-	public void addWeightTask(Task task);
+	
+	//태스크 생성
+	public void createTask(Task task);
+	
+//	// 태스크 추가
+//	public void addCardioTask(Task task);
+//	// 태스크 추가
+//	public void addWeightTask(Task task);
 	// 태스크 삭제
 	public boolean deleteTask(int taskId);
-	// 태스크 수정
-	public void updateCardioTask(Task task);
-	// 태스크 수정
-	public void updateWeightTask(Task task);
+	
+	//태스크 수정
+	public void updateTask(Task task);
+	
+//	// 태스크 수정
+//	public void updateCardioTask(Task task);
+//	// 태스크 수정
+//	public void updateWeightTask(Task task);
 	// 순서 수정
 	public void updateOrder(Task task);
 
